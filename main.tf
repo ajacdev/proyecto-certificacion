4237c4b59ba500580ca0869e5e8edd94b45d4179
Alvaro Juica <alvaro.juica@gmail.com>
2024-11-16 13:12:09 -0300

    Se agrega provider terraform para docker

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0"  
+    }
+  }
+}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
+
 resource "docker_image" "proyecto_certificacion_image" {
   name = "proyecto-certificacion"
   build {
     context = "."
   }
 }
+
 resource "docker_container" "proyecto_certificacion_container" {
   image = docker_image.proyecto_certificacion_image.latest
   name  = "proyecto-certificacion"
